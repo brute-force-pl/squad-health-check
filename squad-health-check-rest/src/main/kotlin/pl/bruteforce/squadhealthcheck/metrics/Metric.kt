91d4af84651ade5f0bbea6b08904b6f637938180
mpogorzelski <mpogorzelski@consdata.pl>
2018-07-22 19:56:43 +0200

    przykłady do dokumentacji

--- squad-health-check-rest/src/main/kotlin/pl/bruteforce/squadhealthcheck/metrics/Metric.kt
+++ squad-health-check-rest/src/main/kotlin/pl/bruteforce/squadhealthcheck/metrics/Metric.kt
@@ -4,5 +4,5 @@ import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("Squad health metric")
-data class Metric(@ApiModelProperty("Identifier of metric") val metricId: Long,
+data class Metric(@ApiModelProperty("Identifier of metric", example = "1") val metricId: Long,
                   @ApiModelProperty("Translations of metric") val translations: List<MetricTranslation>)
--- squad-health-check-rest/src/main/kotlin/pl/bruteforce/squadhealthcheck/metrics/MetricTranslation.kt
+++ squad-health-check-rest/src/main/kotlin/pl/bruteforce/squadhealthcheck/metrics/MetricTranslation.kt
@@ -4,9 +4,9 @@ import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("Squad health metric translation")
-data class MetricTranslation(@ApiModelProperty("Metric translation identifier") val translationId: Long,
-                             @ApiModelProperty("Language of translations") val lang: String,
-                             @ApiModelProperty("Translation of metric name") val name: String,
-                             @ApiModelProperty("Translation of metric green light description") val greenDescription: String,
-                             @ApiModelProperty("Translation of metric red light description") val redDescription: String
+data class MetricTranslation(@ApiModelProperty("Metric translation identifier", example = "1") val translationId: Long,
+                             @ApiModelProperty("Language of translations", example = "en") val lang: String,
+                             @ApiModelProperty("Translation of metric name", example = "Delivering value") val name: String,
+                             @ApiModelProperty("Translation of metric green light description", example = "We deliver great stuff! We're proud of it and our stakeholders are really happy.") val greenDescription: String,
+                             @ApiModelProperty("Translation of metric red light description", example = "We deliver crap. We feel ashamed to deliver it. Our stakeholders hate us.") val redDescription: String
 )
